f9e60fe8ea9165cd2eeb1df54ef4ce6506f8af94
Dima Minka <cdk@cdk.co.il>
2017-08-25 01:45:08 +0300

    feat(easyengine-vagrant): different default

--- easyengine.sh
+++ easyengine.sh
@@ -6,11 +6,9 @@ function myecho()
 	echo =========================================================	
 }
 
-sudo echo -e "[user]\n\tname = EasyEngine\n\temail = root@easyengine.com" > ~/.gitconfig 
-
 myecho
 
-wget -qO ee rt.cx/ee && sudo bash ee stable || exit 1
+wget -qO ee rt.cx/ee && sudo bash ee || exit 1
 
 myecho
 
@@ -18,6 +16,6 @@ sudo ee stack install || exit 1
 
 myecho
 
-sudo ee site create easyengine.com --wpfc || exit 1
+sudo ee site create a.dev --html || exit 1
 
 myecho
